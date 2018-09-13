762ebc6f02598f4957dd5e0165a9e3daa0d5c76a
Giacomo Corallini <corallinigiacomo89@gmail.com>
2018-09-13 15:54:56 +0200

    alcune modifiche di prova

--- aspnet-mvc-template-angular/src/app/app.component.ts
+++ aspnet-mvc-template-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aspnet-mvc-template-angular';
+  title = 'ASP.NET MVC + Angular';
 }
