e3fb987f9b846917b0bee093755631e292d56c4e
Diego Bonura <diego@studiograficod2.it>
2018-10-07 19:51:17 +0200

    Enable really lazy loading

--- angular-cli/src/app/app.routing.ts
+++ angular-cli/src/app/app.routing.ts
@@ -4,21 +4,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 // A little bit lazy Lazy
-import { Module1Module } from '../module1/module1.module';
-import { Module2Module } from '../module2/module2.module';
+//import { Module1Module } from '../module1/module1.module';
+//import { Module2Module } from '../module2/module2.module';
 
-const routes: Routes = [
-  { path: 'component1', loadChildren: () => Module1Module },
-  { path: 'component2', loadChildren: () => Module2Module },
-  { path: '**', component: PageNotFoundComponent }
-];
-
-// // Con lazy
 // const routes: Routes = [
-//   { path: 'component1', loadChildren: './../module1/module1.module#Module1Module'},
-//   { path: 'component2', loadChildren: './../module2/module2.module#Module2Module'},
+//   { path: 'component1', loadChildren: () => Module1Module },
+//   { path: 'component2', loadChildren: () => Module2Module },
 //   { path: '**', component: PageNotFoundComponent }
 // ];
+
+// Con lazy
+const routes: Routes = [
+  { path: 'component1', loadChildren: './../module1/module1.module#Module1Module'},
+  { path: 'component2', loadChildren: './../module2/module2.module#Module2Module'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 // No Lazy
 //import { Component1Component } from '../module1/component1/component1.component';
